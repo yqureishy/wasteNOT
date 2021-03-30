36a844ff277ace308d74986157e442ba44a5bedb
hao <haodam87@gmail.com>
2021-03-30 17:16:30 -0500

    usersdonation

--- models/fooddonation.js
+++ models/fooddonation.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // FoodDonation.hasOne(User, {as:'donor'});
+      FoodDonation.hasOne(User, {as:'donor'});
       // define association here
     }
   };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-// const models = require('../models')
+const models = require('../models')
 
 
 
@@ -9,9 +9,9 @@ router.get('/profile', (req, res) => {
     res.render('profile')
 })
 
-router.get('/donation', (req, res) => {
-    res.render('profile')
-})
+// router.get('/donation', (req, res) => {
+//     res.render('profile')
+// })
 // add new food donation to DB
 router.post('/donation', async (req, res) => {
     const itemName = req.body.itemName
