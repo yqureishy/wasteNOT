04a28a06077373254590fef3ed06c5d060080842
yqureishy <yqureishy1993@gmail.com>
2021-03-30 16:19:48 -0500

    donation linked to fooddonation database

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,29 @@ router.get('/profile', (req, res) => {
     res.render('profile')
 })
 
+
+// add new food donation to DB
+router.post('/donation', async (req, res) => {
+    const itemName = req.body.itemName
+    const estimatedQty = parseInt(req.body.estimatedQty)
+    const estimatedExpiration = req.body.estimatedExpiration
+    const isReadyToEat = req.body.isReadyToEat
+    const storageTemp = req.body.storageTemp
+    const userId = req.session.user.userId
+    let foodDonation = models.Fooddonation.build({
+        itemName: itemName,
+        estimatedQty: estimatedQty,
+        estimatedExpiration: estimatedExpiration,
+        isReadyToEat: isReadyToEat,
+        storageTemp: storageTemp,
+        userId: userId
+    })
+    let persistedProduct = await foodDonation.save()
+    if(persistedProduct != null) {
+        res.redirect('/users/profile')
+    }else {
+    res.redirect('/users/profile', {message: 'Thank you for your donation! One of our volunteers will arrive shortly to pick it up.'})
+    }
+})
+
 module.exports = router
