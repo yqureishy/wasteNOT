26b583194486ba20ca8cdb87ddb503aee0b89ecc
yqureishy <yqureishy1993@gmail.com>
2021-03-31 09:41:17 -0500

    stash

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(FoodDonation, {as: 'FoodDonations'});
+      // User.hasMany(FoodDonation, {as: 'FoodDonations'});
 
       // define association here
     }
