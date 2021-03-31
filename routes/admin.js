bc129248813e89da3d994ff97f0aad934f42f249
hao <haodam87@gmail.com>
2021-03-31 12:16:35 -0500

    stash

--- routes/admin.js
+++ routes/admin.js
@@ -10,8 +10,43 @@ router.get('/add-admin', (req, res) => {
     res.render('add-admin')
 })
 
+// add new admin user
+router.post('/add-admin', async (req, res) => {
 
+    const emailAsUsername = req.body.emailAsUsername;
+    let password = req.body.password
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
 
+    const salt = await bcrypt.genSalt(10)
+    let hashedPassword = await bcrypt.hash(password, salt)
+    let registeredAdmin = await models.Admins.findOne({
+        where:{
+            emailAsUsername: emailAsUsername
+        }
+    })
+    if(!registeredAdmin){
+       
+        let newAdmin = models.Admins.build({
+            emailAsUsername: emailAsUsername,           
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
+
+        let savedAdmin = await newAdmin.save()
+
+    if(savedAdmin != null) {
+        res.render('login', {newAdminMessage: 'New admin saved successfully!' })
+    }else{
+        res.render('add-admin',{message:"Username already exists."})
+    }
+
+    } else{
+        res.render('add-admin',{message:"Username already exists."})
+    }
+
+})
 
 
 
